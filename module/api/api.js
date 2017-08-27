20707533db0542f814dd9d5f9c42c909c78f9aac
Akshay <akshaychauhan7737@gmail.com>
2017-08-27 18:16:55 +0530

    delete node modules from git

--- module/api/api.js
+++ module/api/api.js
@@ -1,9 +1,24 @@
+<<<<<<< Updated upstream
 var express = require('express');
 var api = express();
 
 api.use('/API',function(req,res,next){
+=======
+var express = require("express");
+var router = express.Router();
+router.use("/getuserDetails", function (req, res, next) {
+>>>>>>> Stashed changes
 
 });
 
 
+<<<<<<< Updated upstream
 module.exports = api;
+=======
+
+
+
+
+
+module.exports = router;
+>>>>>>> Stashed changes
